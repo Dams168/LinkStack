0631568c50bbf5194e44c7dbac25bddafe9ee65b
Dams168 <adamrohman2003@gmail.com>
2025-12-24 10:19:05 +0700

    fix: display can track analytic clicks

--- blocks/youtube/display.blade.php
+++ blocks/youtube/display.blade.php
@@ -1,14 +1,18 @@
-<div class="fadein w-full">
-    <span class="text-sm font-medium">
-                {{ $link->title }}
-            </span>
-        <div class="relative w-full overflow-hidden rounded-xl aspect-video">
+{{-- call Endpoint clickNumber for analytic clicks --}}
+<a href="{{ route('clickNumber', $link->id) }}" class="block">
+    <div class="fadein w-full">
+        {{-- For title URL --}}
+        <span class="text-sm font-medium">
+            {{ $link->title }}
+        </span>
+
+        {{-- Display video Base on URL --}}
+        <div class="relative w-full overflow-hidden rounded-xl aspect-video pointer-events-none">
             <iframe
                 class="absolute inset-0 h-full w-full"
                 src="{{ $link->link }}"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
                 loading="lazy">
             </iframe>
         </div>
-</div>
+    </div>
+</a>
