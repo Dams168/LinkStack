efeb6f9a60e34ad6032681bdbee4914ad087f764
Dams168 <adamrohman2003@gmail.com>
2025-12-29 15:55:14 +0700

    add: safetylink column in links table

---
+++ database/migrations/2025_12_29_030739_add_safety_link.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->enum('safety_status',['block','safe','warning'])->default('safe');
+            $table->integer('safety_score')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('links', function (Blueprint $table) {
+            //
+        });
+    }
+};
