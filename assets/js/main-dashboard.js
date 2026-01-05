cd73ba820ed768fd1a83b04d8630d94d9f82e09f
rihapfirdaus <muhammadrihap448@gmail.com>
2026-01-05 07:01:04 +0700

    fix: update shortable configuration

--- assets/js/main-dashboard.js
+++ assets/js/main-dashboard.js
@@ -1,49 +1,45 @@
-
 (function ($) {
-
     "use strict";
 
     var fullHeight = function () {
-
-        $('.js-fullheight').css('height', $(window).height());
+        $(".js-fullheight").css("height", $(window).height());
         $(window).resize(function () {
-            $('.js-fullheight').css('height', $(window).height());
+            $(".js-fullheight").css("height", $(window).height());
         });
-
     };
     fullHeight();
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
+    $("#sidebarCollapse").on("click", function () {
+        $("#sidebar").toggleClass("active");
     });
 
     $.ajaxSetup({
         headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
+            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+        },
     });
     var sortableTbody = document.getElementById("links-table-body");
     if (sortableTbody) {
         const sortableLinkTable = Sortable.create(sortableTbody, {
             handle: ".sortable-handle",
             animation: 150,
-            swapThreshold: 0.60,
-            ghostClass: 'bg-soft-secondary',
-            onChange: function (event) {
-            },
+            swapThreshold: 0.6,
+            ghostClass: "bg-soft-secondary",
+            dataIdAttr: "data-id",
+            onChange: function (event) {},
             store: {
                 get: function (sortable) {
                     var order = linksTableOrders || "";
-                    return order ? order.split('|') : [];
+                    return order ? order.split("|") : [];
                 },
                 set: function (sortable) {
                     const linkOrders = sortable.toArray();
                     const currentPage = sortableTbody.dataset.page || 1;
                     const perPage = sortableTbody.dataset.perPage || 0;
                     const formData = {
-                        'linkOrders': linkOrders,
-                        'currentPage': currentPage,
-                        'perPage': perPage,
+                        linkOrders: linkOrders,
+                        currentPage: currentPage,
+                        perPage: perPage,
                     };
                     // $.blockUI({
                     // 	message: '<img width="70px" src="img/loading.gif" />',
@@ -58,28 +54,34 @@
                     var str = window.location.pathname;
                     str = str.replace("/studio/links", "");
 
-                    $.post(str + "/studio/sort-link", formData, function (response) {
-                        if (response.linkOrders) {
-                            $.each(response.linkOrders, function (linkId, linkOrder) {
-                                // $("#links-table-body div[data-id='"+linkId+"']")
-                                // 	.find("div:eq(3)")
-                                // 	.html(linkOrder);
-                            });
-                            //$.unblockUI();
-                        } else {
-                            alert("Something went wrong! Please, Try again.")
+                    $.post(
+                        str + "/studio/sort-link",
+                        formData,
+                        function (response) {
+                            if (response.linkOrders) {
+                                $.each(
+                                    response.linkOrders,
+                                    function (linkId, linkOrder) {
+                                        // $("#links-table-body div[data-id='"+linkId+"']")
+                                        // 	.find("div:eq(3)")
+                                        // 	.html(linkOrder);
+                                    }
+                                );
+                                //$.unblockUI();
+                            } else {
+                                alert(
+                                    "Something went wrong! Please, Try again."
+                                );
+                            }
                         }
-                    });
-                    
-                    setTimeout(function(){
-                        document.getElementById('frPreview1').src += '';
-                        document.getElementById('frPreview2').src += '';
+                    );
+
+                    setTimeout(function () {
+                        document.getElementById("frPreview").src += "";
+                        // document.getElementById('frPreview2').src += '';
                     }, 300);
-                }
-            }
+                },
+            },
         });
     }
-
-
-
 })(jQuery);
